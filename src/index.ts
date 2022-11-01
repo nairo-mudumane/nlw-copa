d625bf11647fb336ce4bdfe3d26b1dfbcdc371d6
nairo-mudumane <nairomudumane@outlook.com>
2022-11-01 09:22:02 +0200

    getting data from database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["error", "info", "query", "warn"],
+});
 
 async function start() {
   const fastify = Fastify({
     logger: true,
+  });
+
+  fastify.get("/pools/count", async () => {
+    const pools = await prisma.pool.findMany({
+      orderBy: { createdAt: "asc" },
+    });
+
+    return { message: "ok", total: pools.length, data: pools };
   });
 
   await fastify.listen({ port: 3333 });
